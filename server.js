65047af3af994f8cd0362fbb75debf29732f37ae
Glitch (progettopdgt-alessiomuzi-meteo) <none>
2022-08-20 16:48:23 +0000

    Fix minore commenti

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// import dei package necessari per lo sviluppo e definizione app
+// import dei package necessari per lo sviluppo e definizione app/variabili di ambiente
 const express = require('express');
 const cookieparser = require('cookie-parser');
 const sha256 = require('js-sha256');
