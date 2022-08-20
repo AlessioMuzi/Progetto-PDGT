5c03918d326a81ef39a30871e8788ecde9a68dd6
Glitch (progettopdgt-alessiomuzi-meteo) <none>
2022-08-20 14:03:06 +0000

    Import dei package

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 // import dei package necessari e definizione app
 const express = require('express');
+const cookieparser = require('cookie-parser');
+const sha256 = require('js-sha256');
+const jwt = require('njwt');
+const cod_segreto = process.env.JWT_SECRET;
 const app = express();
+app.use(express.json());
+app.use(cookieparser());
